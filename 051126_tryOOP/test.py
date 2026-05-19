11352f1eb3d843580b25c0bd223f3e359f149c0f
JProject <jproyekto@gmail.com>
2026-05-19 20:43:53 +0800

    create json data

--- 051126_tryOOP/test.py
+++ 051126_tryOOP/test.py
@@ -1,1 +1,18 @@
-print("test")
+import json
+
+#load data
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+# menu_items = ", ".join(data["menu_items"].keys())
+
+print(f"Welcome to Candy Shop")
+
+for m in data["menu_items"]:
+    print(m)
+
+user_choice = input("Enter your order:").lower()
+print(user_choice)
+
+print(data["menu_items"][user_choice]["cost"])
+
