1faebdf553543d8ef680f3e69326f8163e274206
JProject <jproyekto@gmail.com>
2026-05-20 14:07:11 +0800

    update

--- 051126_tryOOP/main.py
+++ 051126_tryOOP/main.py
@@ -1,11 +1,17 @@
 import json
+import process
+
+from datetime import datetime
+
 
 #load data
 with open("data.json", "r", encoding="utf8") as f:
     data = json.load(f)
 # declare
 menu_items = data["menu_items"]
+resources = data["resources"]
 currency = data["CURRENCY"]
+profit = data["profit"]
 
 
 def main():
@@ -17,7 +23,14 @@ def main():
         for item_name, item_data in menu_items.items():
             print(f"{currency}{item_data["cost"]} {item_name}")
         print("``````````````````````````````````````````````")
-
+        u_order = process.getOrder()
+        if u_order in ("x", "exit", "off", "quit"):
+            is_on = False
+            break
+        elif u_order in ("p", "print", "r", "report"):
+            print(f"\n|\tProfit {currency}{profit} as of {datetime.now()}\t|\n")
+        else:
+            processOrder = process.is_available(u_order,menu_items,resources)
 
 
 
--- 051126_tryOOP/process.py
+++ 051126_tryOOP/process.py
@@ -3,5 +3,13 @@ class Process:
     def __init__(self):
         pass
 
-    def getOrder(self):
-        user_order = input("Enter your ord")
+
+def getOrder():
+    user_order = input("Enter your order: ").lower()
+    return user_order
+
+def is_available(u_order, m_items, resrs):
+    """
+    check the resources if user order is available
+    """
+    pass
